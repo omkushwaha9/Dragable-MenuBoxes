c23dba2b09e71f7181ae915063db158283394bd5
omkushwaha9 <omkushwaha2005@gmail.com>
2024-07-24 21:52:44 +0530

    Font familt

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function App() {
   return (
     <div className='w-full h-screen bg-zinc-800'>
-      <h1></h1>
+      <h1 className='text-[14vw] ' >Docs</h1>
     </div>
   )
 }
