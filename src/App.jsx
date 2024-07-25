d88bba7547fcaf6a41c48337ec773dd8d2554590
omkushwaha9 <omkushwaha2005@gmail.com>
2024-07-25 14:02:21 +0530

    Background

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 function App() {
   return (
     <div className='relative w-full h-screen bg-zinc-800'>
-      <h1 className=' absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] text-[14vw] leading-none -tracking-tighter' >Docs</h1>
+      <div className=' absolute w-full py-10  top-[5%] flex justify-center font-semibold text-zinc-600 text-xl '> Documents.</div>
+      <h1 className=' absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] text-[14vw] leading-none -tracking-tighter font-semibold text-zinc-900'  >Docs.</h1>
     </div>
   )
 }
