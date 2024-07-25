196eb311b4bd5f3d1ccda01a478da5c37817ed38
omkushwaha9 <omkushwaha2005@gmail.com>
2024-07-25 13:36:06 +0530

    Changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 function App() {
   return (
-    <div className='w-full h-screen bg-zinc-800'>
-      <h1 className='text-[14vw] ' >Docs</h1>
+    <div className='relative w-full h-screen bg-zinc-800'>
+      <h1 className=' absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] text-[14vw] leading-none -tracking-tighter' >Docs</h1>
     </div>
   )
 }
