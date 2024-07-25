9d0a4e382c18dca36ebf9117f41a9844ad613f7f
omkushwaha9 <omkushwaha2005@gmail.com>
2024-07-25 15:24:59 +0530

    Card 1 Completed

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import { FaRegFileAlt } from "react-icons/fa";
+import { LuDownload } from "react-icons/lu";
 
 function Card() {
   return (
-    <div className='w-60 h-72 rounded-[12px] bg-zinc-900/90 '>
-      
+    <div className=' relative w-60 h-72 rounded-[45px] bg-zinc-900/90 text-white px-7 py-7 overflow-hidden'>
+      <FaRegFileAlt/>
+      <p className='text-sm mt-5 leading-tight font-semibold'>Lorem, ipsum dolor sit amet consectetur adipisicing.</p>
+      <div className='footer absolute bottom-0 w-full left-0 '>
+      <div className='  flex items-center justify-between mb-1 px-5 py-3'>
+        <h5>.4mb</h5>
+        <span className='w-7 h-7 bg-zinc-600 rounded-full flex items-center justify-center'>
+        <LuDownload size=".7em" color='#fff' />
+        </span>
+      </div>
+      <div className='tag w-full py-4 bg-green-500'>
+       <h3 className='text-md flex justify-center font-semibold'> Download Now </h3>
+      </div>
+      </div>
     </div>
   )
 }
